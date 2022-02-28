a3ae5845b3be6560a6323a69d0892f380f5ee243
Andrei-Florin Ilișoi <95552031+Dego12@users.noreply.github.com>
2022-02-28 15:57:27 +0200

    first commit

---
+++ src/main/java/com/dego/awscoolsite/AwsCoolSiteApplication.java
@@ -0,0 +1,13 @@
+package com.dego.awscoolsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCoolSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCoolSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dego/awscoolsite/AwsCoolSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dego.awscoolsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCoolSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
